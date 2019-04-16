19bc7bf451411139119b1636bc067f5c80daa838
Gavin Quirk <gavinquirk@gmail.com>
2019-04-16 15:34:57 -0700

    successfull file uploading and alerts

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,25 +1,57 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+
+import Message from './Message';
 
 const FileUpload = () => {
+  // React Hooks instead of component state
   const [file, setFile] = useState();
   const [fileName, setFileName] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
 
   const onChange = e => {
     setFile(e.target.files[0]);
     setFileName(e.target.files[0].name);
   };
 
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart-form-data'
+        }
+      });
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data.msg);
+      }
+    }
+  };
+
   return (
     <Fragment>
-      <form>
-        <div class='custom-file mb-4'>
+      {message ? <Message msg={message} /> : null}
+      <form onSubmit={onSubmit}>
+        <div className='custom-file mb-4'>
           <input
             type='file'
-            class='custom-file-input'
+            className='custom-file-input'
             id='customFile'
             onChange={onChange}
           />
-          <label class='custom-file-label' htmlFor='customFile'>
+          <label className='custom-file-label' htmlFor='customFile'>
             {fileName}
           </label>
         </div>
@@ -29,6 +61,18 @@ const FileUpload = () => {
           className='btn btn-primary btn-block mt-4'
         />
       </form>
+      {uploadedFile.fileName ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img
+              style={{ width: '100%' }}
+              src={uploadedFile.filePath}
+              alt='uploaded image'
+            />
+          </div>
+        </div>
+      ) : null}
     </Fragment>
   );
 };
